7067aeee301b8a8479b708903f7bf6ce2a27c07e
shelfens <stefanie.helfenstein@epfl.ch>
2024-05-07 10:50:08 +0200

    voilaaaa

--- compute_mixed_sample_density_normal_distribution.R
+++ compute_mixed_sample_density_normal_distribution.R
@@ -1,26 +1,21 @@
 #clear all variables :
 rm(list = ls())
 
-#library for traingular distribution :
-library(triangle)
-library(EnvStats)
-
 #####################HAZARD IDENTIFICATION##################################################
-#####################Definition of parameters###############################################
 
-# create dataframe of concentration => made by the data of PETTERSON ET AL, 2016 [ Log10 of organisms / g of feces] 
-# Those are numbers following a triangular distribution => needs to be adapted
+# create dataframe of concentration => made by the data of SCH�NNING ET AL, 2006 [ Ln of organisms / g of feces] 
+# Those are numbers following a normal distribution
 
-# Data for duration of excretion [days]
+# Data for duration of excretion [days], ln
 duration_excretion <- data.frame(
   salm = c(3.6, 0.2),
   rota = c(1.6, 1.23),
   crypto = c(2.0,0.85),
   giardia = c(4.5, 0.7),
   EHEC = c(2.1, 0.25)
-)n
+)
 
-# Data for Excretion density [nb of org/g of feces]
+# Data for Excretion density [nb of org/g of feces], ln
 excretion_density <- data.frame(
   salm = c(13.8, 2.3),
   rota = c(20.7,2.3),
@@ -29,7 +24,7 @@ excretion_density <- data.frame(
   EHEC = c(5.8, 1.2)
 )
 
-# Data for incidences (same soruce as trinangular distribution) [ ]
+# Data for incidences [ ]
 inc_df <- data.frame(
   salm = c(500/100000,100/100000),
   rota = c(1200/100000,200/100000),
@@ -37,10 +32,10 @@ inc_df <- data.frame(
   giardia = c(1100/100000,100/100000),
   EHEC = c(30/100000, 5/100000)
 )
-#!!!!!!!!!!!!!!!!!!!!Check for all normal distributions, that you put log into not log!!!!!!!!!!!!!!!!
+
 # Other parameters
 X <- 500000             # fictive population size    
-rate <- 250             # [g/cap/day] 
+rate <- 250             # [g/cap/day] #Study of TO-FIGUERAS ET AL.(2000), BALCELLS GORINA (1989), SCHOUW ET AL.(2002)
 
 pathogens <- c("salm", "rota", "crypto", "giardia", "EHEC")
 
@@ -52,21 +47,24 @@ prod_infected_pop_non_infected_feces_list <- list()
 
 #####################Production of feces, infected and non infected############################
 
-# Yearly production of non-infected feces by the non infected population :
-prod_no_infected_pop <- rate*365*(1-inc_df)*X # [g/year] 
-
-# Yearly production of infected feces by the infected population :
 # Set parameters for Duration [days]
 mean_duration <- duration_excretion[1,]   # Minimum value
 sd_duration <- duration_excretion[2,] # Mode (most probable value)
-#n <- inc_df[1,]*X  # Number of random numbers to generate (= infected population)
 
+n <- inc_df[1,]*X # this is a generalization: we took the mean without taking into consideration the standard deviation. this will be changed towards a distribution in a further step
+
+prod_no_infected_pop <- list()
 durations <- list()
 for (pathogen in pathogens) {
-  n <- rnorm(X, mean = inc_df[1, pathogen], sd = inc_df[2,pathogen])*X
-  durations[[pathogen]] <- ceiling((rnorm(n, mean_duration[1, pathogen], sd_duration[1, pathogen])))
+  # Production of solid excreta of the non infected population => will be represented as well as a distribution on a further step
+  prod_no_infected_pop[[pathogen]] <- mean(rate*365*(1-rnorm(X, mean = inc_df[1, pathogen], sd = inc_df[2,pathogen]))*X) # [g/year] 
+  
+  # Calculates the durations
+  durations[[pathogen]] <- (rnorm(n[1, pathogen], mean_duration[1, pathogen], sd_duration[1, pathogen])) # [days/year]
+  durations[[pathogen]] = ceiling(exp(durations[[pathogen]]))
 }
 
+# Yearly production of infected and non-infected feces by the infected population :
 prod_infected_pop_infected_feces_list <- list()
 prod_infected_pop_non_infected_feces_list <- list()
 
@@ -82,104 +80,78 @@ for (pathogen in names(durations)) {
 }
 
 
-#  Total yealy quantity of uninfected feces :
-tot_non_infected_feces <- prod_infected_pop_non_infected_feces+prod_no_infected_pop
+#Calculation of total non infected feaces
+tot_non_infected_feaces <- list()
+
+for (pathogen in pathogens) {
+  tot_non_infected_feaces[[pathogen]] <- prod_infected_pop_non_infected_feces_list[[pathogen]]+prod_no_infected_pop[[pathogen]]
+}
 
 
 #####################Density of microorganisms in the feaces#####################################
 
-# Generate an excretion density for each excretion of each person
-
+# Calculation of the number of pathogens in the infected feaces
 nb_microb_in_infected_feces_list <- list()
 
-min_density <- excretion_density[1,]   # Minimum value
-max_density <- excretion_density[3,]  # Maximum value
-mode_density <- excretion_density[2,]  # Mode (most probable value)
+mean_density <- excretion_density[1,]   # Mean
+sd_density <- excretion_density[2,]   # Standard deviation
 
-#####Not that sure, that it is working the right way. To check. ####
-for (pathogen in names(durations)) { #for each pathogen
+for (pathogen in names(durations)) {
   nb_microb_in_infected_feces <- 0
-  for (duration in durations[[pathogen]]) {  # Change "campylo" to [pathogen]
+  for (duration in durations[[pathogen]]) {
     n <- duration
-    random_density <- (10^(rtriangle(n, min_density[1, pathogen], max_density[1, pathogen], mode_density[1, pathogen])))
+    random_density <- (exp(rnorm(n, mean_density[1, pathogen], sd_density[1, pathogen])))
     for (density in random_density) {
       nb_microb_in_infected_feces <- nb_microb_in_infected_feces + rate * density
     }
   }
-  nb_microb_in_infected_feces_list[[pathogen]] <- nb_microb_in_infected_feces
-  print(nb_microb_in_infected_feces)#[nb of org in the yearly production of infected feces]
-}
-
+  nb_microb_in_infected_feces_list[[pathogen]] <- nb_microb_in_infected_feces 
+} # [nb of pathogens in the yearly production of infected feces]
 
 
 # Find the average pathogen density of each pathogen
 average_pathogen_density_list <- list()
 
-# Loop through each pathogen
 for (pathogen in names(nb_microb_in_infected_feces_list)) {
-  # Perform element-wise division and store the result in the result_list
-  average_pathogen_density_list[[pathogen]] <- nb_microb_in_infected_feces_list[[pathogen]] / (tot_non_infected_feces[[pathogen]] + prod_infected_pop_infected_feces_list[[pathogen]])
-} #[nb org excreted / year ÷ g feces/ year] => [nb. org.  / g feces in mixed population]
-# The multiplication by 10^3 is to go from g to mg 
+  average_pathogen_density_list[[pathogen]] <- 
+    nb_microb_in_infected_feces_list[[pathogen]] / (tot_non_infected_feaces[[pathogen]] + prod_infected_pop_infected_feces_list[[pathogen]])
+} # [nb org excreted / year ÷ g feces/ year] => [nb. org.  / g feces in mixed population]
 
 
 
+#####################EXPOSURE PATHWAY#####################################
 
-
-#####################EXPOSURE PATHWAY#########################################################
-
-#Transfer rates as indicated in Brooks
+# Transfer rates as indicated in Brooks: 
+## The parameters here are an approximation of their distribution. 
+## In the final code, this parameters will be in form of probability distribution
 t_hm <- 0.36 #[]
-t_fh <- 0.43
-t_gf <- 0.27 #to be changed
-#a_gf <- (0.1+0.17)/2*420
-t_exg <- (0.16+0.28)/2 #[mg/cm^2]
-a_gex <- (0.13+0.25)/2*420 #[cm^2]
+t_fh <- 0.43 # []
+t_gf <- 0.27 # []
+t_exg <- (0.16+0.28)/2 # [mg/cm^2]
+a_gex <- (0.13+0.25)/2*420 # [cm^2]
 
-#g ingested
 ingestion_g <- t_exg*a_gex*t_fh*t_hm*t_gf
 
-# N pathogens ingested
+# compute the number of pathogens ingested
 n_pathogens_ingested <- list()
 
-# Loop through each pathogen
 for (pathogen in names(average_pathogen_density_list)) {
-  # Perform element-wise division and store the result in the result_list
-  n_pathogens_ingested[[pathogen]] = ingestion_g * average_pathogen_density_list[[pathogen]]/1000
-  print('hi')
+  n_pathogens_ingested[[pathogen]] = 
+    ingestion_g * average_pathogen_density_list[[pathogen]]/1000
 } #[nb org excreted / year ÷ g feces/ year] => [nb. org.  / mg feces in mixed population]
-# division by 1000 is to go from g to mg in order to have consistent units. 
-
-#something dont work here. it shoulld diminish by 10^(-5), no? whats wrong?
 
 #####################DOSE-RESPONSE###########################################################
 
-# Data for dose response model
-param_beta_poisson <- data.frame(
-  rota = c(0.26, 0.42), # c(alpha, beta)
-  campylo1996 = c(0.145, 7.59),
-  campylo2005 = c(0.024, 0.011)
-)
-
-param_exponential <- data.frame(
-  adeno = 4.172*10^(-1),
-  crypto = 0.0042
-)
-
-
+pathogens <- c("salm", "rota", "crypto", "giardia", "EHEC")
 
 # Exponential model
-response_exponential_adeno = 1-exp(-4.172*10^(-1)*n_pathogens_ingested[["adeno"]]) #Dose needs to be adjusted to the INGESTED DOSE
-response_exponential_crypto = 1-exp(-0.0042*n_pathogens_ingested[["crypto"]]) 
+response_exponential_crypto <- 1-exp(-0.0042*n_pathogens_ingested[["crypto"]]) 
+response_exponential_giardia <- 1-exp(-0.0198*n_pathogens_ingested[["giardia"]]) 
 
 # Beta poisson
-response_beta_poisson_rota = 1-(1+n_pathogens_ingested[["rota"]]/(0.26))^(-0.42) #Dose needs to be adjusted to the INGESTED DOSE
-response_beta_poisson_campylo1996 = 1-(1+n_pathogens_ingested[["campylo"]]/(0.145))^(-7.59) #Campylo jejuni
-response_beta_poisson_campylo2005 = 1-(1+n_pathogens_ingested[["campylo"]]/(0.024))^(-0.011)
-response_beta_poisson_salmonellanontyphoide = 1-(1+n_pathogens_ingested[["salm"]]*(2^(1/(0.21))-1)/49.8)^(-0.21)
-
-#####################RISK CHARACTERIZATION##################################################
-
+response_beta_poisson_rota = 1-(1+n_pathogens_ingested[["rota"]]/(0.42))^(-0.26) #Dose needs to be adjusted to the INGESTED DOSE
+response_beta_poisson_salmonellanontyphoide <- 1-(1+n_pathogens_ingested[["salm"]]*(2^(1/(0.21))-1)/49.8)^(-0.21)
+response_beta_poisson_EHEC <- 1-(1+n_pathogens_ingested[["EHEC"]]/(39.71))^(-0.373)
 
 
    
--- compute_mixed_sample_density_triangular_distribution.R
+++ compute_mixed_sample_density_triangular_distribution.R
@@ -6,10 +6,9 @@
   library(EnvStats)
 
 #####################HAZARD IDENTIFICATION##################################################
-#####################Definition of parameters###############################################
-  
+
 # create dataframe of concentration => made by the data of PETTERSON ET AL, 2016 [ Log10 of organisms / g of feces] 
-# Those are numbers following a triangular distribution => needs to be adapted
+# Those are numbers following a triangular distribution
   
 # Data for duration of excretion [days]
   duration_excretion <- data.frame(
@@ -20,7 +19,7 @@
     crypto = c(5,10,30)
   )
   
-# Data for Excretion density [nb of org/g of feces]
+# Data for Excretion density [nb of org/g of feces], log10
   excretion_density <- data.frame(
     adeno = c(8,10,12),
     rota = c(8,10,12),
@@ -29,7 +28,7 @@
     crypto = c(6,7,9)
   )
 
-# Data for incidences (same soruce as trinangular distribution) [ ]
+# Data for incidences (same source as trinangular distribution) [ ]
   inc_df <- data.frame(
     adeno = 0.88/100,
     rota = 1.28/100,
@@ -44,33 +43,31 @@ rate <- 250             # [g/cap/day]
 
 pathogens <- c("adeno", "rota", "campylo", "salm", "crypto")
 
-
 # Initialize lists to store results for each pathogen
 prod_infected_pop_infected_feces_list <- list()
 prod_infected_pop_non_infected_feces_list <- list()
 
 
-#####################Production of feces, infected and non infected############################
+#############Production of feces, infected and non infected##################
 
 # Yearly production of non-infected feces by the non infected population :
 prod_no_infected_pop <- rate*365*(1-inc_df)*X # [g/year] 
 
 # Yearly production of infected feces by the infected population :
-# Set parameters for Duration [days]
 min_duration <- duration_excretion[1,]   # Minimum value
 max_duration <- duration_excretion[3,]  # Maximum value
 mode_duration <- duration_excretion[2,]  # Mode (most probable value)
 n <- inc_df[1,]*X  # Number of random numbers to generate (= infected population)
 
+durations <- list()
+for (pathogen in pathogens) {
+  # Calculates the durations
+  durations[[pathogen]] <- (rtriangle(n[1, pathogen], min_duration[1, pathogen], max_duration[1, pathogen], mode_duration[1,pathogen])) # [days/year]
+  durations[[pathogen]] = ceiling(durations[[pathogen]])
+}
 
-durations <- list(
-  rota = ceiling(rtriangle(n[1,'rota'], min_duration[1, 'rota'], max_duration[1, 'rota'], mode_duration[1,'rota'])),
-  adeno = ceiling(rtriangle(n[1,'adeno'], min_duration[1, 'adeno'], max_duration[1, 'adeno'], mode_duration[1,'adeno'])),
-  campylo = ceiling(rtriangle(n[1,'campylo'], min_duration[1, 'campylo'], max_duration[1, 'campylo'], mode_duration[1,'campylo'])),
-  salm = ceiling(rtriangle(n[1,'salm'], min_duration[1, 'salm'], max_duration[1, 'salm'], mode_duration[1,'salm'])),
-  crypto = ceiling(rtriangle(n[1,'crypto'], min_duration[1, 'crypto'], max_duration[1, 'crypto'], mode_duration[1,'crypto']))
-)
 
+# Yearly production of infected and non-infected feces by the infected population :
 prod_infected_pop_infected_feces_list <- list()
 prod_infected_pop_non_infected_feces_list <- list()
 
@@ -85,25 +82,23 @@ for (pathogen in names(durations)) {
   prod_infected_pop_non_infected_feces_list[[pathogen]] <- prod_infected_pop_non_infected_feces
 }
 
-
 #  Total yealy quantity of uninfected feces :
 tot_non_infected_feces <- prod_infected_pop_non_infected_feces+prod_no_infected_pop
 
 
-#####################Density of microorganisms in the feaces#####################################
 
-# Generate an excretion density for each excretion of each person
+################Density of microorganisms in the feaces#######################
 
+# Compute the number of pathogens in the infected feaces
 nb_microb_in_infected_feces_list <- list()
    
 min_density <- excretion_density[1,]   # Minimum value
 max_density <- excretion_density[3,]  # Maximum value
 mode_density <- excretion_density[2,]  # Mode (most probable value)
 
-#####Not that sure, that it is working the right way. To check. ####
-for (pathogen in names(durations)) { #for each pathogen
+for (pathogen in names(durations)) { 
   nb_microb_in_infected_feces <- 0
-  for (duration in durations[[pathogen]]) {  # Change "campylo" to [pathogen]
+  for (duration in durations[[pathogen]]) {  
     n <- duration
     random_density <- (10^(rtriangle(n, min_density[1, pathogen], max_density[1, pathogen], mode_density[1, pathogen])))
     for (density in random_density) {
@@ -111,32 +106,23 @@ for (pathogen in names(durations)) { #for each pathogen
     }
   }
   nb_microb_in_infected_feces_list[[pathogen]] <- nb_microb_in_infected_feces
-  print(nb_microb_in_infected_feces)#[nb of org in the yearly production of infected feces]
-}
-
+} # [nb of org in the yearly production of infected feces]
 
 
 # Find the average pathogen density of each pathogen
 average_pathogen_density_list <- list()
 
-# Loop through each pathogen
 for (pathogen in names(nb_microb_in_infected_feces_list)) {
-  # Perform element-wise division and store the result in the result_list
   average_pathogen_density_list[[pathogen]] <- nb_microb_in_infected_feces_list[[pathogen]] / (tot_non_infected_feces[[pathogen]] + prod_infected_pop_infected_feces_list[[pathogen]])
 } #[nb org excreted / year ÷ g feces/ year] => [nb. org.  / g feces in mixed population]
-# The multiplication by 10^3 is to go from g to mg 
-
-
-
 
 
 #####################EXPOSURE PATHWAY#########################################################
 
 #Transfer rates as indicated in Brooks
 t_hm <- 0.36 #[]
-t_fh <- 0.43
-t_gf <- 0.27 #to be changed
-#a_gf <- (0.1+0.17)/2*420
+t_fh <- 0.43 #[]
+t_gf <- 0.27 #[]
 t_exg <- (0.16+0.28)/2 #[mg/cm^2]
 a_gex <- (0.13+0.25)/2*420 #[cm^2]
 
@@ -146,47 +132,19 @@ ingestion_g <- t_exg*a_gex*t_fh*t_hm*t_gf
 # N pathogens ingested
 n_pathogens_ingested <- list()
 
-# Loop through each pathogen
 for (pathogen in names(average_pathogen_density_list)) {
-  # Perform element-wise division and store the result in the result_list
   n_pathogens_ingested[[pathogen]] = ingestion_g * average_pathogen_density_list[[pathogen]]/1000
-  print('hi')
 } #[nb org excreted / year ÷ g feces/ year] => [nb. org.  / mg feces in mixed population]
-# division by 1000 is to go from g to mg in order to have consistent units. 
 
-#something dont work here. it shoulld diminish by 10^(-5), no? whats wrong?
 
 #####################DOSE-RESPONSE###########################################################
-
-# Data for dose response model
-param_beta_poisson <- data.frame(
-  rota = c(0.26, 0.42), # c(alpha, beta)
-  campylo1996 = c(0.145, 7.59),
-  campylo2005 = c(0.024, 0.011)
-)
-
-param_exponential <- data.frame(
-  adeno = 4.172*10^(-1),
-  crypto = 0.0042
-)
-
-
 
 # Exponential model
 response_exponential_adeno = 1-exp(-4.172*10^(-1)*n_pathogens_ingested[["adeno"]]) #Dose needs to be adjusted to the INGESTED DOSE
 response_exponential_crypto = 1-exp(-0.0042*n_pathogens_ingested[["crypto"]]) 
 
 # Beta poisson
-response_beta_poisson_rota = 1-(1+n_pathogens_ingested[["rota"]]/(0.26))^(-0.42) #Dose needs to be adjusted to the INGESTED DOSE
-response_beta_poisson_campylo1996 = 1-(1+n_pathogens_ingested[["campylo"]]/(0.145))^(-7.59) #Campylo jejuni
-response_beta_poisson_campylo2005 = 1-(1+n_pathogens_ingested[["campylo"]]/(0.024))^(-0.011)
-response_beta_poisson_salmonellanontyphoide = 1-(1+n_pathogens_ingested[["salm"]]*(2^(1/(0.21))-1)/49.8)^(-0.21)
-###########!!!!!!!!!!!!!!!Attention, samonella probablement not real route, check!!!!!!!!!!!
-#####################RISK CHARACTERIZATION##################################################
-
-
-  
-  
-  
-
+response_beta_poisson_rota = 1-(1+n_pathogens_ingested[["rota"]]/(0.42))^(-0.26) #Dose needs to be adjusted to the INGESTED DOSE
+response_beta_poisson_campylo1996 = 1-(1+n_pathogens_ingested[["campylo"]]/(7.59))^(-0.145) #Campylo jejuni
+response_beta_poisson_campylo2005 = 1-(1+n_pathogens_ingested[["campylo"]]/(0.011))^(-0.024)
   
